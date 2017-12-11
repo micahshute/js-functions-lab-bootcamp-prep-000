0eaedf498a512995d9f402a786ab12c42ba809c0
Micah Shute <micah.shute@gmail.com>
2017-12-11 02:06:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,19 @@
 // function holidayCountdown(holiday, days){
 //   return `It's ${days} days until ${holiday}!`
 // }
+
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`
+}
